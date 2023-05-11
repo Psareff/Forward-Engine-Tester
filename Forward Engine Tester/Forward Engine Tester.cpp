1185e44d97429266f348e449ed7d0189534ece2e
Psareff <ilyapsareff@gmail.com>
2023-05-12 01:43:40 +0700

    Finale

--- Forward Engine Tester/Forward Engine Tester.cpp
+++ Forward Engine Tester/Forward Engine Tester.cpp
@@ -8,33 +8,41 @@
 int main()
 {
 #pragma region initial_values
-
 	TorqueFromSpeed tfs({ {0, 20}, {75, 75}, {150, 100}, {200, 105}, {250, 75}, {300, 0} });
-
 	EngineCharacteristics ec;
+
 	ec.moment_of_inertia = 10;
 	ec.heating_from_speed_coef = 0.0001;
 	ec.heating_from_torque_coef = 0.01;
 	ec.cooling_from_ambient_coef = 0.1;
 	ec.overheat_temperature = 110;
-
 #pragma endregion
-
 	double ambient_temperature;
+	try {
 
-	std::cout << "Enter ambient temperature: ";
-	std::cin >> ambient_temperature;
+		std::cout << "Enter ambient temperature: ";
+		std::cin >> ambient_temperature;
 
-	IEngine* engine = new InternalCombustionEngine(ec, tfs, ambient_temperature);
+		if (ambient_temperature > ec.overheat_temperature)
+			throw std::invalid_argument("Ambient temperature is higher than engine overheat");
 
-	engine->resetEngine();
-	IEngineTest<double>* ot = new OverheatTest<double>(engine);
-	std::cout << "Overheat time: " << ot->run(1) << " s" << std::endl << std::endl;
+		IEngine* engine = new InternalCombustionEngine(ec, tfs, ambient_temperature);
+		engine->resetEngine();
 
-	engine->resetEngine();
-	IEngineTest<std::pair<double, double>>* mpt = new MaxPowerTest<std::pair<double, double>>(engine);
-	std::pair<double, double> res = mpt->run(1);
-	std::cout << "Max power: " << res.first << " kW ";
-	std::cout << "at torque: " << res.second << " Nm" << std::endl;
+		// Overheat test, stops at 110 C
+		IEngineTest<double>* ot = new OverheatTest<double>(engine);
+		std::cout << "Overheat time: " << ot->run(1) << " s" << std::endl << std::endl;
 
+		engine->resetEngine();
+
+		// Mex power test, stops at maximum power reached
+		IEngineTest<std::pair<double, double>>* mpt = new MaxPowerTest<std::pair<double, double>>(engine);
+		std::pair<double, double> res = mpt->run(1);
+		std::cout << "Max power: " << res.first << " kW ";
+		std::cout << "at torque: " << res.second << " Nm" << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what();
+	}
 }
--- Forward Engine Tester/InternalCombustionEngine/src/TorqueFromSpeed.cpp
+++ Forward Engine Tester/InternalCombustionEngine/src/TorqueFromSpeed.cpp
@@ -1,7 +1,6 @@
 #pragma once
 #include "../include/TorqueFromSpeed.hpp"
 #include <stdexcept>
-
 TorqueFromSpeed::TorqueFromSpeed(std::map<double, double> initial_dependency)
 	: initial_dependency_(initial_dependency) {}
 
@@ -10,11 +9,14 @@ double TorqueFromSpeed::operator()(double crankshaft_speed)
 	if (crankshaft_speed > (--initial_dependency_.end())->first ||
 		crankshaft_speed < (initial_dependency_.begin())->first)
 		throw std::invalid_argument("Crankshaft speed is out of bounds");
+
+	// Piecewise linear interpolation of dependency of torque on crankshaft speed
 	else if (initial_dependency_.find(crankshaft_speed) == initial_dependency_.end())
 	{
 		auto it = initial_dependency_.lower_bound(crankshaft_speed);
 		std::pair<double, double> next = *it--, previous = *it;
 		return previous.second + (next.second - previous.second) * (crankshaft_speed - previous.first) / (next.first - previous.first);
 	}
+
 	else return initial_dependency_[crankshaft_speed];
 }
